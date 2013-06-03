1b3280e73c82c7b9a02c66dab86a27f21e593b57
Ron Rechenmacher <ron@fnal.gov>
2013-06-03 17:01:39 -0500

    more clean/rm

--- xrawdata0/.xrawdata0.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/ron/work/mu2ePrj/PCIe_module/k7_pcie_dma_ddr3_base/linux_driver/xrawdata0/xrawdata0.ko := cp /home/ron/work/mu2ePrj/PCIe_module/k7_pcie_dma_ddr3_base/linux_driver/xrawdata0/xrawdata0.ko.unsigned /home/ron/work/mu2ePrj/PCIe_module/k7_pcie_dma_ddr3_base/linux_driver/xrawdata0/xrawdata0.ko
--- xrawdata1/.built-in.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/ron/work/mu2ePrj/PCIe_module/k7_pcie_dma_ddr3_base/linux_driver/xrawdata1/built-in.o :=  rm -f /home/ron/work/mu2ePrj/PCIe_module/k7_pcie_dma_ddr3_base/linux_driver/xrawdata1/built-in.o; ar rcs /home/ron/work/mu2ePrj/PCIe_module/k7_pcie_dma_ddr3_base/linux_driver/xrawdata1/built-in.o
