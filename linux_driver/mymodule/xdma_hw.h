ab2515297b48d0fd1ebb29a1b4b48c120fae0e35
Ron Rechenmacher <ron@fnal.gov>
2014-02-12 14:05:43 -0600

    delete unused lines

--- linux_driver/mymodule/xdma_hw.h
+++ linux_driver/mymodule/xdma_hw.h
@@ -9,236 +9,14 @@ extern "C" {
 #include <asm/io.h>
 #endif
 
-
-#ifndef TRUE
-#  define TRUE    1
-#endif
-
-#ifndef FALSE
-#  define FALSE   0
-#endif
-
-#ifndef NULL
-#define NULL    0
-#endif
-
-
-#define XCOMPONENT_IS_READY     0x11111111  /**< Component has been initialized */
-#define XCOMPONENT_IS_STARTED   0x22222222  /**< Component has been started */
-
-/* The following constants and declarations are for unit test purposes and are
- * designed to be used in test applications.
- */
-#define XTEST_PASSED    0
-#define XTEST_FAILED    1
-
-#define XASSERT_NONE     0
-#define XASSERT_OCCURRED 1
-
-extern unsigned int XAssertStatus;
-extern void XAssert(char *, int);
-
-/**************************** Type Definitions *******************************/
-
-/** @name Legacy types
- * Deprecated legacy types.
- * @{
- */
-typedef unsigned char  Xuint8;   /**< unsigned 8-bit */
-typedef char           Xint8;    /**< signed 8-bit */
-typedef unsigned short Xuint16;  /**< unsigned 16-bit */
-typedef short          Xint16;   /**< signed 16-bit */
-typedef unsigned       Xuint32;  /**< unsigned 32-bit */
-typedef int            Xint32;   /**< signed 32-bit */
-typedef float          Xfloat32; /**< 32-bit floating point */
-typedef double         Xfloat64; /**< 64-bit double precision FP */
-typedef unsigned       Xboolean; /**< boolean (XTRUE or XFALSE) */
-
-typedef struct
-{
-  Xuint32 Upper;
-  Xuint32 Lower;
-} Xuint64;
-
-/** @name New types
- * New simple types.
- * @{
- */
 #ifndef __KERNEL__
-typedef Xuint32         u32;
-typedef Xuint16         u16;
-typedef Xuint8          u8;
+typedef unsigned char      u8;
+typedef unsigned short     u16;
+typedef unsigned int       u32;
+typedef unsigned long long u64;
 #else
 #include <linux/types.h>
 #endif
-
-/*@}*/
-
-/**
- * This data type defines an interrupt handler for a device.
- * The argument points to the instance of the component
- */
-typedef void (*XInterruptHandler) (void *InstancePtr);
-
-/**
- * This data type defines an exception handler for a processor.
- * The argument points to the instance of the component
- */
-typedef void (*XExceptionHandler) (void *InstancePtr);
-
-/**
- * This data type defines a callback to be invoked when an
- * assert occurs. The callback is invoked only when asserts are enabled
- */
-typedef void (*XAssertCallback) (char *FilenamePtr, int LineNumber);
-
-/***************** Macros (Inline Functions) Definitions *********************/
-
-/*****************************************************************************/
-/**
-* Return the most significant half of the 64 bit data type.
-*
-* @param    x is the 64 bit word.
-*
-* @return   The upper 32 bits of the 64 bit word.
-*
-* @note     None.
-*
-******************************************************************************/
-#define XUINT64_MSW(x) ((x).Upper)
-
-/*****************************************************************************/
-/**
-* Return the least significant half of the 64 bit data type.
-*
-* @param    x is the 64 bit word.
-*
-* @return   The lower 32 bits of the 64 bit word.
-*
-* @note     None.
-*
-******************************************************************************/
-#define XUINT64_LSW(x) ((x).Lower)
-
-
-#ifndef NDEBUG
-
-/*****************************************************************************/
-/**
-* This assert macro is to be used for functions that do not return anything
-* (void). This in conjunction with the XWaitInAssert boolean can be used to
-* accomodate tests so that asserts which fail allow execution to continue.
-*
-* @param    expression is the expression to evaluate. If it evaluates to
-*           false, the assert occurs.
-*
-* @return   Returns void unless the XWaitInAssert variable is true, in which
-*           case no return is made and an infinite loop is entered.
-*
-* @note     None.
-*
-******************************************************************************/
-#define XASSERT_VOID(expression)                   \
-{                                                  \
-    if (expression)                                \
-    {                                              \
-        XAssertStatus = XASSERT_NONE;              \
-    }                                              \
-    else                                           \
-    {                                              \
-        XAssert(__FILE__, __LINE__);               \
-                XAssertStatus = XASSERT_OCCURRED;  \
-        return;                                    \
-    }                                              \
-}
-
-/*****************************************************************************/
-/**
-* This assert macro is to be used for functions that do return a value. This in
-* conjunction with the XWaitInAssert boolean can be used to accomodate tests so
-* that asserts which fail allow execution to continue.
-*
-* @param    expression is the expression to evaluate. If it evaluates to false,
-*           the assert occurs.
-*
-* @return   Returns 0 unless the XWaitInAssert variable is true, in which case
-*           no return is made and an infinite loop is entered.
-*
-* @note     None.
-*
-******************************************************************************/
-#define XASSERT_NONVOID(expression)                \
-{                                                  \
-    if (expression)                                \
-    {                                              \
-        XAssertStatus = XASSERT_NONE;              \
-    }                                              \
-    else                                           \
-    {                                              \
-        XAssert(__FILE__, __LINE__);               \
-                XAssertStatus = XASSERT_OCCURRED;  \
-        return 0;                                  \
-    }                                              \
-}
-
-/*****************************************************************************/
-/**
-* Always assert. This assert macro is to be used for functions that do not
-* return anything (void). Use for instances where an assert should always
-* occur.
-*
-* @return Returns void unless the XWaitInAssert variable is true, in which case
-*         no return is made and an infinite loop is entered.
-*
-* @note   None.
-*
-******************************************************************************/
-#define XASSERT_VOID_ALWAYS()                      \
-{                                                  \
-   XAssert(__FILE__, __LINE__);                    \
-           XAssertStatus = XASSERT_OCCURRED;       \
-   return;                                         \
-}
-
-/*****************************************************************************/
-/**
-* Always assert. This assert macro is to be used for functions that do return
-* a value. Use for instances where an assert should always occur.
-*
-* @return Returns void unless the XWaitInAssert variable is true, in which case
-*         no return is made and an infinite loop is entered.
-*
-* @note   None.
-*
-******************************************************************************/
-#define XASSERT_NONVOID_ALWAYS()                   \
-{                                                  \
-   XAssert(__FILE__, __LINE__);                    \
-           XAssertStatus = XASSERT_OCCURRED;       \
-   return 0;                                       \
-}
-
-
-#else
-
-#define XASSERT_VOID(expression)
-#define XASSERT_VOID_ALWAYS()
-#define XASSERT_NONVOID(expression)
-#define XASSERT_NONVOID_ALWAYS()
-#endif
-
-/************************** Function Prototypes ******************************/
-
-void XAssertSetCallback(XAssertCallback Routine);
-void XNullHandler(void *NullParameter);
-
-
-
-/**
- * Typedef for an I/O address.  Typically correlates to the width of the
- * address bus.
- */
-typedef Xuint32 XIo_Address;
 
 
 /* NWL DMA design is little-endian, so values need not be swapped.
@@ -254,9 +32,6 @@ typedef Xuint32 XIo_Address;
         writel((data), (unsigned int *)(addr));      \
     })
 
-#ifndef __KERNEL__
-typedef unsigned long long u64;
-#endif
 
 
 //#define MAX_DMA_ENGINES         64      /**< Maximum number of DMA engines */
@@ -474,11 +249,6 @@ static unsigned long mu2e_ch_reg_offset[2][2] ={ {0x2000,0x0}, {0x2100,0x100} };
 
 /**************************** Type Definitions *******************************/
 
-/***************** Macros (Inline Functions) Definitions *********************/
-
-#ifdef TH_BH_ISR
-#define INT_COAL_CNT        16  /* Interrupt coalesce count */
-#endif
 
 /*****************************************************************************/
 /**
