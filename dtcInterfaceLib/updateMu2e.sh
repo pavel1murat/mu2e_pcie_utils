f7b702a5ef993aaf19a1a3701a93ea5ee20e3f71
Eric Flumerfelt <eflumerf@fnal.gov>
2015-01-27 11:32:10 -0600

    Updated updateMu2e.sh so it should work if its run from the package build directory.

--- dtcInterfaceLib/updateMu2e.sh
+++ dtcInterfaceLib/updateMu2e.sh
@@ -1,6 +1,8 @@
-source ./build.sh
+if [[ ! -f DTC.node ]]; then
+  source ./build.sh
+fi
 ssh mu2edaq01.dhcp "mkdir -p ~/dev/modules/DTC/server"
-scp build/Debug/DTC.node mu2edaq01.dhcp:~/dev/modules/DTC/server
+scp DTC.node mu2edaq01.dhcp:~/dev/modules/DTC/server
 scp -r web/modules/* mu2edaq01.dhcp:~/dev/modules
 scp web/serverbase.js mu2edaq01.dhcp:~/dev
 scp web/client.html mu2edaq01.dhcp:~/dev
