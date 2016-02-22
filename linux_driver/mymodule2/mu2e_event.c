085604b727a610d815a2ec9f8d1c64e788be0100
MU2E <mu2edaq@mu2edaq01.fnal.gov>
2016-02-22 12:21:16 -0600

    Move printing to a differnt TRACE level.

--- linux_driver/mymodule2/mu2e_event.c
+++ linux_driver/mymodule2/mu2e_event.c
@@ -61,7 +61,7 @@ static void poll_packets(unsigned long __opaque)
 			TRACE(0, "poll_packets: newCmpltIdx (0x%lx) is above maximum sane value!!! (%lu) Current idx=0x%x", newCmpltIdx, MU2E_NUM_RECV_BUFFS, mu2e_channel_info_[chn][dir].hwIdx);
 			continue;
 		}
-		TRACE(0, "poll_packets: newCmpltIdx=0x%lx MU2E_NUM_RECV_BUFFS=%lu Current idx=0x%x", newCmpltIdx, MU2E_NUM_RECV_BUFFS, mu2e_channel_info_[chn][dir].hwIdx);
+		TRACE(20, "poll_packets: newCmpltIdx=0x%lx MU2E_NUM_RECV_BUFFS=%lu Current idx=0x%x", newCmpltIdx, MU2E_NUM_RECV_BUFFS, mu2e_channel_info_[chn][dir].hwIdx);
 		// check just-read-HW-val (converted to idx) against "cached" copy
 		while (newCmpltIdx != mu2e_channel_info_[chn][dir].hwIdx/*ie.cachedCmplt*/)
 		{   // NEED TO UPDATE Receive Byte Counts
