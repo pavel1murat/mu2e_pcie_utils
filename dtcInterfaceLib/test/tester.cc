6b9c51cdf7eb645c644f3c2b095625634ddf5e9e
Eric Flumerfelt <eflumerf@fnal.gov>
2016-04-05 09:59:58 -0500

    Fix an infinite wait. tester was setting sim file flags even if there wasn't one.

--- dtcInterfaceLib/test/tester.cc
+++ dtcInterfaceLib/test/tester.cc
@@ -64,7 +64,9 @@ int main(int argc, char* argv[])
 	TRACE(1, "simFile is %s", simFile.c_str());
 	DTC_SimMode mode = DTCLib::DTC_SimModeConverter::ConvertToSimMode(std::to_string(modeint));
 	DTC* thisDTC = new DTC(mode);
-	thisDTC->WriteSimFileToDTC(simFile, true, true);
+	if (simFile.size() > 0) {
+		thisDTC->WriteSimFileToDTC(simFile, true, true);
+	}
 	TRACE(1, "thisDTC->ReadSimMode: %i", thisDTC->ReadSimMode());
 	DTCSoftwareCFO* theCFO = new DTCSoftwareCFO(thisDTC, true);
 	long loopCounter = 0;
