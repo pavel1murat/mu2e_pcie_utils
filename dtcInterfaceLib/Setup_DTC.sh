f97651bd269d77c4797b8d2a44e901aa751aa6b6
Eric Flumerfelt <eflumerf@fnal.gov>
2020-11-11 16:37:33 -0600

    Wait for SERDES reset done

--- dtcInterfaceLib/Setup_DTC.sh
+++ dtcInterfaceLib/Setup_DTC.sh
@@ -86,7 +86,15 @@ DTC_Reset()
   my_cntl write 0x9100 0x80000000  >/dev/null # reset DTC  reset serdes osc
   my_cntl write 0x9118 0x000000ff  >/dev/null  # SERDES resets
   my_cntl write 0x9100 0x00000000  >/dev/null  # clear reset
-  sleep 1
+
+  counter=0  
+  while [ `my_cntl read 0x9138|grep 0x` != 0xffffffff ];do
+	echo "Waiting for SERDES Reset done, $counter seconds elapsed"
+    sleep 1
+    counter=$(( $counter + 1 ))
+  done
+
+
   my_cntl write 0x9118 0x00000000  >/dev/null  # clear SERDES reset on link 0
 }
 
