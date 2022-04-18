e0f30976267c2cd4acbc9d0d55894414b8968033
Eric Flumerfelt <eflumerf@fnal.gov>
2022-04-18 11:24:41 -0500

    Update make_test_file.sh script

--- tools/make_test_file.sh
+++ tools/make_test_file.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-source /mu2e/ups/setup
-source /cvmfs/mu2e.opensciencegrid.org/artexternals/setups
-
-setup pcie_linux_kernel_module v2_05_14 -qe20:s114:prof # Update as needed
-
 # DTC Config
 export DTCLIB_SIM_ENABLE=E
 export DTCLIB_NUM_TRACKER_BLOCKS=212
@@ -13,4 +8,5 @@ export DTCLIB_NUM_CALORIMETER_HITS=10
 export DTCLIB_NUM_CRV_BLOCKS=0
 
 # Create file
-mu2eUtil -H -n 1000 --binary-file-mode DTC_packets.bin
+mu2eUtil -Q -n 1000 --binary-file-mode DTC_packets.bin verify_stream
+
